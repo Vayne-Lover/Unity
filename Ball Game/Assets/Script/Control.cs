3115b5b4059baf11c28c74fec9e81a27f81a3763
Vayne-Lover <406378362@qq.com>
2016-05-21 00:52:30 +0800

    Add Codes

---
+++ Ball Game/Assets/Script/Control.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Control : MonoBehaviour {
+    public float force = 20.0f;
+    private Rigidbody rigid;
+	// Use this for initialization
+	void Start () {
+        rigid = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.y < 0)
+        {
+            Destroy(gameObject);
+            Application.Quit();
+        }
+	}
+    void OnCollisionEnter(Collision collision) {
+        rigid.AddForce(new Vector3(Random.Range(-1.5f, 1.5f), 2.0f, 0) * force);
+    }
+}
---
+++ Ball Game/Assets/Script/Move.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+    public float speed = 5.0f;
+	void Update () {
+        float horizontal = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right*horizontal*speed*Time.deltaTime);
+	}
+}
